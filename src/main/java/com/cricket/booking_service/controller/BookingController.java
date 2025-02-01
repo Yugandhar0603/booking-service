39ecc9b0d86547a39b5bd1b9f3d14c19b67dd386
Vishrutth <vishrutth5214@gmail.com>
2025-02-01 16:59:52 +0530

    again

--- src/main/java/com/cricket/booking_service/controller/BookingController.java
+++ src/main/java/com/cricket/booking_service/controller/BookingController.java
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 import com.cricket.booking_service.entity.Booking;
 import com.cricket.booking_service.pojo.BookingDTO;
+import com.cricket.booking_service.pojo.BookingTeamPlayersPojo;
+import com.cricket.booking_service.pojo.ScoresPojo;
+import com.cricket.booking_service.pojo.TeamPojo;
 import com.cricket.booking_service.service.BookingService;
 
 @RestController
@@ -29,7 +33,6 @@ public class BookingController {
         BookingDTO bookingDTO = bookingService.getBookingsById(bookingId);
         return ResponseEntity.ok(bookingDTO);
     }
-
     @PostMapping
     public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
         return new ResponseEntity<Booking>(bookingService.createBooking(booking),HttpStatus.OK);
@@ -37,10 +40,39 @@ public class BookingController {
 
     @GetMapping("/get/{id}")
     public ResponseEntity<Optional<Booking>> getBookingById(@PathVariable int id) {
-        return new ResponseEntity<Optional<Booking>>(bookingService.getBookingById(id),HttpStatus.OK);
+        return new ResponseEntity<Optional<Booking>>(bookingService.getById(id),HttpStatus.OK);
+    }
+	// 	    SlotPojo slot = restClient
+	// 			.get()
+	// 			.uri("http://localhost:1719/slots/"+id)
+	// 			.retrieve()
+	// 			.body(SlotPojo.class);
+    @GetMapping("/wholescore/{bookingId}")
+    public ResponseEntity<BookingTeamPlayersPojo> getWholeScore(int bookingId){
+        BookingTeamPlayersPojo wholeScore;
+        RestClient restClient = RestClient.create();
+        List<TeamPojo> teams=restClient
+         .get()
+         .uri("http://localhost:8090/booking/"+bookingId)
+         .retrieve()
+         .body(List.class)
+         List<ScoresPojo> scores=restClient
+         .get()
+         .uri("http://localhost:8086")
+         .retrieve()
+         .body(List.class)
+         Booking booking=bookingService.getById(bookingId).orElse(null);
+         wholeScore.setBookingId(bookingId);
+         wholeScore.setDate(booking.getBookingDate());
+         wholeScore.setGameSwitch(false);
+         wholeScore.setGameWinTeam(0);
+         wholeScore.setTeams(teams);
+         wholeScore.setScores(scores);
+         return new ResponseEntity<>(wholeScore,HttpStatus.OK);
+         
     }
 
-    @GetMapping
+    @GetMapping()
     public List<Booking> getAllBookings() {
         return bookingService.getAllBookings();
     }
---
+++ src/main/java/com/cricket/booking_service/pojo/ScoresPojo.java
@@ -0,0 +1,18 @@
+package com.cricket.booking_service.pojo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoresPojo {
+    private int gameTotalOvers;
+    private int strikerRuns;
+    private int wideBall;
+    private int gameWickets;
+    private int gameBallNo;
+    private int strikerPlayerId;
+    private int nonStrikerPlayerId;
+    private int bowlerId;
+}
